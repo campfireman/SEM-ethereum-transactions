614ad41565da55f9782531ee963882f611958291
Ture <ture.cl.business@gmail.com>
2020-05-27 15:51:46 +0200

    updates

--- scripts/intrinsic_cost.py
+++ scripts/intrinsic_cost.py
@@ -6,6 +6,8 @@ BYTE_IN_HEX = 2
 
 
 def main():
+    data_zero = 0
+    data_nonzero = 0
     data = str(input('please enter the value of the data field as hex string: '))
     byte_list = [data[i:i+BYTE_IN_HEX]
                  for i in range(0, len(data), BYTE_IN_HEX)]
@@ -16,10 +18,14 @@ def main():
             raise ValueError('{} not a byte'.format(byte))
 
         if byte == '00':
+            data_zero += 1
             gzero += G_DATA_ZERO
         else:
+            data_nonzero += 1
             gzero += G_DATA_NONZERO
 
+    print('data zero: {}'.format(data_zero))
+    print('data nonzero: {}'.format(data_nonzero))
     print('intrinsic cost: {}'.format(gzero))
 
 
